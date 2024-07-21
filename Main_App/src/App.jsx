d173f4b53be082a0d1e1bc8042db818f920b5b65
VirajB7 <busaviraj743@gmail.com>
2024-07-21 16:39:39 +0530

    Frontend setup

---
+++ Main_App/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='w-screen h-screen bg-red-300'>
+        <h1>React app</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
