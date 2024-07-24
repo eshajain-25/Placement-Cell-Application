6282714f61089c232a849f6d0012411e2417c9fe
VirajB7 <114498949+VirajB7@users.noreply.github.com>
2024-07-24 13:25:07 +0530

    Delete Main_App/src directory
    Deleted due to many difference in file name conventions

--- Main_App/src/dataFiles/studentsData.js
+++
@@ -1,7 +0,0 @@
-export const studentsData = [
-    {id: 1, name: 'John Doe', age: 15, gender: 'Male', branch: 'CSE', status: 'Placed', dsaScore: 90 },
-    {id: 2, name: 'Jane Smith', age: 14, gender: 'Male', branch: 'ECE', status: 'Not Placed', dsaScore: 80 },
-    {id:3, name: 'Samantha Johnson', age: 16, gender: 'Female', branch: 'CSE', status: 'Placed', dsaScore: 70 },
-    // Add more student objects here
-  ];
-  
